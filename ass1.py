b4921876b71e81f2cd7c23a79d21417754f7082a
harika <85933172+bitlabswq-456@users.noreply.github.com>
2022-01-04 17:25:57 +0530

    Add files via upload

---
+++ ass1.py
@@ -0,0 +1,9 @@
+import numpy as np
+#create array numpy.array()
+#array object is called ndarray
+arr=np.array([12,78,90,67])
+print(arr)
+#typeof array object
+print(type(arr))
+#dimension array
+print(arr.ndim)
---
+++ ass2.py
@@ -0,0 +1,5 @@
+import numpy as np
+arr=np.array([1,2,3,4,5,6,78,9,10])
+addition=arr[1]*arr[1]+arr[3]*arr[3]+arr[5]*arr[5]+arr[7]*arr[7]
+print(addition)
+
