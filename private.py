6ee1f3bb8b3a9a9d3e34d7d205d5ec724c79393d
harika <85933172+bitlabswq-456@users.noreply.github.com>
2022-01-06 10:00:34 +0530

    Add files via upload

---
+++ private.py
@@ -0,0 +1,20 @@
+#program for private variables
+
+class ABC:
+
+    __name="bitlabs"
+
+    def m1(self):
+
+        print(self.__name)
+
+class XYZ(ABC):
+
+    def m2(self):
+
+        print("hi")
+
+obj=XYZ()
+
+obj.m2()
+        
---
+++ protected.py
@@ -0,0 +1,23 @@
+#program for protected variables
+
+class ABC:
+
+    _name="bitlabs"
+
+    def m1(self):
+
+        print(self._name)
+
+class XYZ(ABC):
+
+    def m2(self):
+
+        print(self._name)
+
+
+obj=XYZ()
+
+obj.m1()
+
+obj.m2()
+        
